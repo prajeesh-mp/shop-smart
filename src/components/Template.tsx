b251e46624cd739465db64e5e2b4f24bbc1181ef
Prajeesh MP <meetprajeesh@gmail.com>
2023-12-02 00:39:22 +0530

    Update: Template structure

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,23 +1,29 @@
 function Template() {
   return (
     <div className="border rounded p-2 px-3 justify-content-center align-items-center my-3">
-      <h5>Kid's Shopping</h5>
+      <h4>Kid's Shopping</h4>
+
+      <h5>Groceries</h5>
 
       <div className="d-flex flex-row justify-content-between align-items-center">
-        <p>Raagi</p>
-        <p className="text-muted">Kg</p>
+        <p>Mushroom</p>
+        <p className="text-muted">250 gm</p>
       </div>
+      <div className="d-flex flex-row justify-content-between align-items-center">
+        <p>Banana</p>
+        <p className="text-muted">500 gm</p>
+      </div>
+
+      <h5>Baby Care</h5>
 
       <div className="d-flex flex-row justify-content-between align-items-center">
-        <p>Milk Powder</p>
-        <p className="text-muted">Kg</p>
+        <p>Diaper</p>
+        <p className="text-muted">5 pcs</p>
       </div>
-
       <div className="d-flex flex-row justify-content-between align-items-center">
-        <p>Milk Powder</p>
-        <p className="text-muted">Kg</p>
+        <p>Milk Bottle</p>
+        <p className="text-muted">1 pcs</p>
       </div>
-
       <hr />
       <div className="d-flex justify-content-center">
         <button className="btn btn-primary btn-sm">use this template</button>
