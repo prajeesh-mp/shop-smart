15b94fbfe4bdf3ecf23d3be7cf0b65a5e804d285
Prajeesh MP <meetprajeesh@gmail.com>
2024-02-15 12:50:37 +0530

    Update: Routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,14 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "../components/Layout";
 import PageNotFound from "../pages/404";
 import PurchaseList from "../pages/PurchaseList";
+import MobileLayout from "../layout/MobileLayout";
+import Welcome from "../pages/Welcome";
+import Dashboard from "../pages/Dashboard";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="*" element={<PageNotFound />} />
 
-      <Route element={<Layout />}>
+      {/* <Route element={<Layout />}>
         <Route path="/" element={<PurchaseList />} />
+      </Route> */}
+      <Route element={<MobileLayout />}>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Route>
     </Routes>
   );
