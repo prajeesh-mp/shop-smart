766837e9b13a459900f43373aba609172e5d0e81
Prajeesh MP <meetprajeesh@gmail.com>
2024-02-22 11:07:02 +0530

    Update: Purchase List Logic

--- src/pages/PurchaseList.tsx
+++ src/pages/PurchaseList.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { FiArrowLeftCircle, FiPlus, FiTrash2 } from "react-icons/fi";
+import { Stores, addData, addMultipleData } from "../services/db.service";
 
 interface Item {
   id: number;
@@ -12,6 +13,7 @@ function PurchaseList(): JSX.Element {
     { id: 1, value: "", fieldRef: null },
   ]);
   const [idCounter, setIdCounter] = useState(1);
+  const [isSaved, setIsSaved] = useState(false);
 
   const handleAddInput = () => {
     const newInputField: Item = { id: idCounter, value: "", fieldRef: null };
@@ -35,6 +37,38 @@ function PurchaseList(): JSX.Element {
     setInputFields(remainingInputs);
   };
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await addData(Stores.PurchaseLists, {
+        name: "Groceries",
+        created_at: Date.now(),
+      });
+
+      console.log(res, "ress");
+      let dd = inputFields.map((item) => ({
+        name: item.value,
+        list_id: res,
+        is_purchased: false,
+        created_at: Date.now(),
+      }));
+
+      console.log(dd);
+
+      const response = await addMultipleData(Stores.ListItems, dd, () => {
+        console.log("Data added !!!");
+        alert("Saved");
+        setIsSaved(true);
+      });
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.log(err.message);
+      } else {
+        console.log("Something went wrong");
+      }
+    }
+  };
+
   useEffect(() => {
     if (inputFields.length > 0 && inputFields[inputFields.length - 1].fieldRef)
       inputFields[inputFields.length - 1].fieldRef?.focus();
@@ -56,59 +90,62 @@ function PurchaseList(): JSX.Element {
           stay within budget, and avoid forgetting items
         </p>
       </div>
+      <form onSubmit={handleSubmit}>
+        <div className="items mt-3">
+          <div className="items-top d-flex flex-row justify-content-between align-items-center my-3">
+            <h2 className="title">Write down</h2>
+            <span className="desc">{inputFields.length} Items</span>
+          </div>
 
-      <div className="items mt-3">
-        <div className="items-top d-flex flex-row justify-content-between align-items-center my-3">
-          <h2 className="title">Write down</h2>
-          <span className="desc">{inputFields.length} Items</span>
-        </div>
-
-        {inputFields &&
-          inputFields.map((input) => (
-            <div
-              className="item d-flex flex-row justify-content-between align-items-center mb-3"
-              key={input.id}
-            >
-              <input
-                name={input.id.toString()}
-                id={input.id.toString()}
-                type="text"
-                className="w-100"
-                placeholder="Write here . ."
-                onChange={(event) => handleInputChange(input.id, event)}
-                onKeyDown={(event) => {
-                  if (event.key === "Enter") {
-                    event.preventDefault();
-                    handleAddInput();
-                  }
-                }}
-                ref={(e) => (input.fieldRef = e)}
-              />
+          {inputFields &&
+            inputFields.map((input) => (
               <div
-                className="trash d-flex flex-row justify-content-center align-items-center"
-                onClick={() => handleRemoveInput(input.id)}
+                className="item d-flex flex-row justify-content-between align-items-center mb-3"
+                key={input.id}
               >
-                <FiTrash2 color="#CD5226" />
+                <input
+                  name={input.id.toString()}
+                  id={input.id.toString()}
+                  type="text"
+                  className="w-100"
+                  placeholder="Write here . ."
+                  onChange={(event) => handleInputChange(input.id, event)}
+                  onKeyDown={(event) => {
+                    if (event.key === "Enter") {
+                      event.preventDefault();
+                      handleAddInput();
+                    }
+                  }}
+                  ref={(e) => (input.fieldRef = e)}
+                />
+                <div
+                  className="trash d-flex flex-row justify-content-center align-items-center"
+                  onClick={() => handleRemoveInput(input.id)}
+                >
+                  <FiTrash2 color="#CD5226" />
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
 
-        <div
-          className="item d-flex flex-row justify-content-center mt-4 mb-5"
-          onClick={() => handleAddInput()}
-        >
-          <FiPlus color="#000" size={20} />
+          <div
+            className="item d-flex flex-row justify-content-center mt-4 mb-5"
+            onClick={() => handleAddInput()}
+          >
+            <FiPlus color="#000" size={20} />
+          </div>
+          <div className="fixed-bottom  btn-container ">
+            <button
+              // label="Save to my list"
+              // isProcessing={false}
+              className={`save-btn ${isSaved && "btn-saved"}`}
+              type="submit"
+              // onClick={handleSubmit}
+            >
+              Save
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="fixed-bottom  btn-container ">
-        <button
-          // label="Save to my list"
-          // isProcessing={false}
-          className="save-btn"
-        >
-          Save
-        </button>
-      </div>
+      </form>
     </div>
   );
 }
