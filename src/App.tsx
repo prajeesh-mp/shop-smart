72e65e3658a4db0b394c45e8051317b612a4ff70
Prajeesh MP <meetprajeesh@gmail.com>
2024-02-22 11:06:48 +0530

    Update: Initiate DB

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Routes from "./routes/Routes";
+import { initDB } from "./services/db.service";
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+
 function App() {
+  const handleInitDB = async () => {
+    const status = await initDB();
+    console.log("status DB", status);
+  };
+
+  useEffect(() => {
+    handleInitDB();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes />
