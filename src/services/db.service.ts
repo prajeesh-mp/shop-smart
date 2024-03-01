881e277d820e02b8981ff3c145a446acaf5c83a5
Prajeesh MP <meetprajeesh@gmail.com>
2024-03-01 18:22:29 +0530

    Update: Indexing

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -18,18 +18,22 @@ export const initDB = (): Promise<boolean> => {
       // if the data object store doesn't exist, create it
       if (!db.objectStoreNames.contains(Stores.PurchaseLists)) {
         console.log("Creating purchase lists store");
-        db.createObjectStore(Stores.PurchaseLists, {
+        let store = db.createObjectStore(Stores.PurchaseLists, {
           keyPath: "id",
           autoIncrement: true,
         });
+
+        store.createIndex("purchase_list_index", "id");
       }
 
       if (!db.objectStoreNames.contains(Stores.ListItems)) {
         console.log("Creating list items store");
-        db.createObjectStore(Stores.ListItems, {
+        let store = db.createObjectStore(Stores.ListItems, {
           keyPath: "id",
           autoIncrement: true,
         });
+
+        store.createIndex("list_items_index", "list_id");
       }
       // no need to resolve here
     };
@@ -130,6 +134,82 @@ export const getStoreData = <T>(storeName: Stores): Promise<T[]> => {
       const res = store.getAll();
       res.onsuccess = () => {
         resolve(res.result);
+      };
+    };
+  });
+};
+
+export const queryStoreData = <T>(
+  storeName: Stores,
+  id: number
+): Promise<T[]> => {
+  return new Promise((resolve, reject) => {
+    var request = indexedDB.open(dbName);
+    request.onsuccess = () => {
+      console.log("request.onsuccess - getQueriedData");
+      db = request.result;
+      const transaction = db.transaction(storeName, "readonly");
+      const store = transaction.objectStore(storeName);
+      const index = store.index("list_items_index");
+
+      var keyRange = IDBKeyRange.only(id);
+      var data: T[] = [];
+
+      index.openCursor(keyRange).onsuccess = function (event: any) {
+        var cursor = event.target?.result;
+
+        if (cursor) {
+          data.push(cursor.value);
+          cursor.continue();
+        } else {
+          resolve(data);
+        }
+      };
+    };
+    request.onerror = () => {
+      reject("Something went wrong");
+    };
+  });
+};
+
+export const updateStoreData = (
+  storeName: Stores,
+  id: number,
+  status: boolean
+): Promise<boolean> => {
+  return new Promise((resolve, reject) => {
+    var request = indexedDB.open(dbName);
+    request.onsuccess = () => {
+      console.log("request.onsuccess - updateData");
+      db = request.result;
+      const transaction = db.transaction(storeName, "readwrite");
+      const store = transaction.objectStore(storeName);
+
+      var getRequest = store.get(id);
+      getRequest.onsuccess = function (event) {
+        var data = getRequest.result;
+
+        if (data) {
+          // Modify the properties of the retrieved object
+          data.is_purchased = status;
+
+          // Put the updated object back into the object store
+          var putRequest = store.put(data);
+
+          putRequest.onsuccess = function (event) {
+            resolve(true);
+          };
+
+          putRequest.onerror = function (event) {
+            reject("Error updating object");
+          };
+        } else {
+          reject("Object not found");
+        }
+      };
+
+      request.onerror = () => {
+        reject("Something went wrong");
       };
     };
   });
